717273ba8e83b123c5d84b9ec55373d3b8da169a
ajj-k <bouando4ha@docomo.ne.jp>
2023-12-19 15:44:49 +0900

    [add] 互いに素な二つの自然数について、約数関数は乗法的

--- Test3.lean
+++ Test3.lean
@@ -1,11 +1,55 @@
--- 約数
+-- 約数のパッケージ
 import Mathlib.NumberTheory.Divisors
--- 集合
-import Mathlib.Data.Finset.Basic
+-- 素数のパッケージ
+-- coprimeはGCDで定義され、インポートされている
+import Mathlib.Data.Nat.Prime
+-- 集合のパッケージ
+-- import Mathlib.Data.Finset.Basic
 
--- 約数の総和の定義
+open Nat
+
+-- Def 1: 約数の総和の定義
 def divisors_sum (n : ℕ) : ℕ :=
-  (Nat.divisors n).sum id
+  (divisors n).sum id
 
 -- 具体例 2の約数の総和は3
-#guard divisors_sum 2 = 3
+#guard coprime 1 2
+#check gcd 1 3
+
+-- 全ての自然数は1を約数に持つ （使わなくても証明できた）
+-- lemma divisor_one : ∀ (n : ℕ), (1 ∣ n) := by
+--   intro n
+--   use n
+--   simp
+
+-- Lem 1: gcd a b = 1 なら、gcd a b は 1 のみで割り切られる
+lemma gcd_one_only_divided_by_one : ∀ (a b : ℕ), (gcd a b = 1) → (1 ∣ gcd a b) ∧ (∀ k > 1, ¬ (k ∣ gcd a b)) := by
+  intros a b h_gcd
+  simp
+  intro k
+  intro k_ineq
+  intro k_gcd
+  rw [h_gcd] at k_gcd
+  cases k
+  {
+    -- k_ineqで矛盾
+    contradiction
+  }
+  {
+    rw [gcd_eq_right_iff_dvd] at k_gcd
+    rw [gcd_comm] at k_gcd
+    rw [gcd_one_right] at k_gcd
+    rw [←k_gcd] at k_ineq
+    exact lt_irrefl 1 k_ineq
+  }
+
+-- Lem 2: 互いに素な二つの自然数について、約数関数は乗法的
+lemma coprime_divisor : ∀ a b : ℕ, (Nat.coprime a b) → ∀ d : ℕ, (d ∣ a ∧ d ∣ b) → d = 1  := by
+  intros a b h_coprime d h_div
+  have h_gcd : Nat.gcd a b = 1 := h_coprime
+  have p := gcd_one_only_divided_by_one a b h_gcd
+  have p_left := p.left
+  rw [←dvd_gcd_iff] at h_div
+  rw [h_gcd] at h_div
+  have d_eq_one : d = 1 := eq_one_of_dvd_one h_div
+  assumption
