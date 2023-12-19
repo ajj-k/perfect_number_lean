710fd247cf4ef6bb5c2fb0c733589e81fb6b502f
ajj-k <bouando4ha@docomo.ne.jp>
2023-12-19 23:55:08 +0900

    [add] lem7.1

--- Test3.lean
+++ Test3.lean
@@ -1,3 +1,4 @@
+import Mathlib.Data.Nat.Basic
 -- 約数のパッケージ
 import Mathlib.NumberTheory.Divisors
 -- 素数のパッケージ
@@ -44,12 +45,23 @@ lemma gcd_one_only_divided_by_one : ∀ (a b : ℕ), (gcd a b = 1) → (1 ∣ gc
   }
 
 -- Lem 2: 互いに素な二つの自然数について、約数関数は乗法的
-lemma coprime_divisor : ∀ a b : ℕ, (Nat.coprime a b) → ∀ d : ℕ, (d ∣ a ∧ d ∣ b) → d = 1  := by
+lemma coprime_divisor : ∀ a b : ℕ, (coprime a b) → ∀ d : ℕ, (d ∣ a ∧ d ∣ b) → d = 1  := by
   intros a b h_coprime d h_div
-  have h_gcd : Nat.gcd a b = 1 := h_coprime
+  have h_gcd : gcd a b = 1 := h_coprime
   have p := gcd_one_only_divided_by_one a b h_gcd
   have p_left := p.left
   rw [←dvd_gcd_iff] at h_div
   rw [h_gcd] at h_div
   have d_eq_one : d = 1 := eq_one_of_dvd_one h_div
   assumption
+
+
+-- Lem 3: 2の累乗 と 奇素数 は互いに素である（これは奇数と2が互いに素であることが言えるため、qが奇素数である条件はいらない）
+lemma coprime_power_of_two_and_odd_prime_numbers : ∀ e q : ℕ, ((Nat.Prime q) ∧ (¬ 2 ∣ q)) → (coprime  (Nat.pow 2 e) q) := by
+  intros e q
+  simp
+  intros h_prime_q h_not_divided_by_2
+  rw [←Nat.Prime.coprime_iff_not_dvd] at h_not_divided_by_2
+  have h_power_of_two : coprime (Nat.pow 2 e) q := coprime.pow_left e h_not_divided_by_2
+  assumption
+  exact prime_two
